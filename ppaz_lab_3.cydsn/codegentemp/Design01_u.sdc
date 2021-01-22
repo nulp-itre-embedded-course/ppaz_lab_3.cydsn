eb28c4fbbef6e8475bf23c257656d78bce6c2797
Yura2112 <yura.on.play@gmail.com>
2021-01-22 17:21:32 +0200

    laba 3

---
+++ ppaz_lab_3.cydsn/codegentemp/Design01_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for E:\Bench\Laba 3\yura01\Design01.cydsn\TopDesign\TopDesign.cysch
+# Project: E:\Bench\Laba 3\yura01\Design01.cydsn\Design01.cyprj
+# Date: Thu, 21 Jan 2021 14:21:53 GMT
---
+++ ppaz_lab_3.cydsn/main.c
@@ -0,0 +1,34 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    UART_1_Start();
+    char buf;
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        buf = UART_1_GetChar();
+        if(0u != buf)
+        {
+        UART_1_PutCRLF(buf);
+        }
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
+
+
